798f8b5608967b372eaff4dc1e4004050ecc0ab7
Pranav Trivedi <pranavaryans@gmail.com>
2019-01-25 12:20:40 +0530

    cerberus

--- build/target/product/AndroidProducts.mk
+++ build/target/product/AndroidProducts.mk
@@ -15,6 +15,6 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/gzosp_arm.mk \
-    $(LOCAL_DIR)/gzosp_arm64.mk \
-    $(LOCAL_DIR)/gzosp_x86.mk
+    $(LOCAL_DIR)/cerberus_arm.mk \
+    $(LOCAL_DIR)/cerberus_arm64.mk \
+    $(LOCAL_DIR)/cerberus_x86.mk
